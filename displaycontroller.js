55ff05a69ec6605a468b9add80447de0d1a45646
Autumn Apsey <autumnapsey@gmail.com>
2016-05-12 09:09:25 -0400

    added all 7 inputs

--- displaycontroller.js
+++ displaycontroller.js
@@ -5,6 +5,8 @@ app.controller('displaycontroller', function($scope, myService) {
 	$scope.string = dataOnPage.string;
 	$scope.string2 = dataOnPage.string2;
 	$scope.string3 = dataOnPage.string3;
-
-	console.log(dataOnPage);
+	$scope.string4 = dataOnPage.string4;
+	$scope.string5 = dataOnPage.string5;
+	$scope.string6 = dataOnPage.string6;
+	$scope.string7 = dataOnPage.string7;
 });
--- inputcontroller.js
+++ inputcontroller.js
@@ -4,13 +4,21 @@ app.controller('inputcontroller', function($scope, myService, $location) {
 	var word = "";
 	var word2 = "";
 	var word3 = "";
-	$scope.getWords = function(wordModel, wordModel2, wordModel3){
+	var word4 = "";
+	var word5 = "";
+	var word6 = "";
+	var word7 = "";
+
+	$scope.getWords = function(wordModel, wordModel2, wordModel3, wordModel4, wordModel5, wordModel6, wordModel7){
 		word += wordModel;
 		word2 += wordModel2;
 		word3 += wordModel3;
+		word4 += wordModel4;
+		word5 += wordModel5;
+		word6 += wordModel6;
+		word7 += wordModel7;
 
-		console.log(word, word2, word3);
-		myService.saveData(word, word2, word3);
+		myService.saveData(word, word2, word3, word4, word5, word6, word7);
 		$location.path('/display');
 	};
 });
